38fbaa42284df1d1f4c72b9c8378284b4955766f
Max von Hippel <maxvonhippel1996@gmail.com>
2017-05-03 10:03:57 -0700

    it works.  Now just need to add a second layer for testing, and get a good system for layer iterating going

--- explorer.js
+++ explorer.js
@@ -1,39 +1,7 @@
 var canvas = document.createElement("canvas");
 // add the canvas
 canvas.setAttribute("id", "cid");
-document.body.appendChild(canvas);
 var context = canvas.getContext('2d');
-
-// declare layers on canvas
-var layer_1 = new Image();  // declare globally
-
-// handle image draw on canvas
-layer_1.onload = function() {
-	window.addEventListener('resize', resizeCanvas, false);
-	resizeCanvas();
-};
-
-layer_1.src = 'photos/layer_1_demo.png';
-
-function resizeCanvas() {
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    drawStuff();
-
-}
-
-// code from: http://stackoverflow.com/a/23105310/1586231
-function drawStuff() {
-
-	var hRatio = canvas.width  / layer_1.width;
-	var vRatio =  canvas.height / layer_1.height;
-	var ratio  = Math.min (hRatio, vRatio);
-	var centerShift_x = (canvas.width - layer_1.width*ratio) / 2;
-	var centerShift_y = (canvas.height - layer_1.height*ratio) / 2;
-	context.drawImage(layer_1, 0, 0, layer_1.width, layer_1.height,
-                      centerShift_x, centerShift_y, layer_1.width*ratio, layer_1.height*ratio);
-}
 
 // handle the clicks
 // code from http://stackoverflow.com/a/38488246/1586231
