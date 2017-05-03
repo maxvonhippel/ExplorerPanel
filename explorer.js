a95c09d9ccc524dd6ce8353a5d07ec64169917a7
Max von Hippel <maxvonhippel1996@gmail.com>
2017-05-03 14:28:52 -0700

    close to done with structure for automatic layer recognition

--- explorer.js
+++ explorer.js
@@ -3,6 +3,16 @@ var canvas = document.createElement("canvas");
 canvas.setAttribute("id", "cid");
 var context = canvas.getContext('2d');
 
+// name with hashtag to find div of a layer
+function layer_name(name) {
+	return '#'.concat(name);
+}
+
+// type name of a javascript obj for debugging
+var toType = function(obj) {
+	return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+}
+
 // function to change list of divs into array
 function toArray(a) {
 	var result = [];
@@ -21,21 +31,27 @@ var images = toArray(container.getElementsByClassName("layer"));
 function assign_callback_for_event(element, event_name) {
 
 	// code from http://stackoverflow.com/a/38488246/1586231
-	var layer_name = '#'.concat(element.id);
-	$(layer_name).on(event_name, function(event) {
+	var _name = layer_name(element.id);
+
+	console.log("assigning callback: " + element.id);
+
+	$(_name).on(event_name, function(event) {
 
 			// Get click coordinates
 			var x = event.pageX - this.offsetLeft,
 				y = event.pageY - this.offsetTop,
 				w = context.canvas.width = this.width,
 				h = context.canvas.height = this.height,
+				rgba,
 				alpha;
 
 			// Draw image to canvas
 			// and read Alpha channel value
 
 			context.drawImage(this, 0, 0, w, h);
-			alpha = context.getImageData(x, y, 1, 1).data[3]; // [0]R [1]G [2]B [3]A
+			rgba = context.getImageData(x, y, 1, 1);
+			console.log("x: " + x + " y: " + y + " w: " + w + " h: " + h + " rgba: " + rgba);
+			alpha = rgba.data[3]; // [0]R [1]G [2]B [3]A
 
 			console.log(alpha);
 
@@ -48,7 +64,7 @@ function assign_callback_for_event(element, event_name) {
 				$(this).show();
 
 			} else {
-				console.log("CLICKED: " + element.id);
+				console.log("CLICKED: " + this.id);
 			}
 
 		});
@@ -57,17 +73,22 @@ function assign_callback_for_event(element, event_name) {
 
 function assign_callback(element, index, array) {
 
-	try {
+	element.onload = function() {
 
-		// try to assign callback
-		assign_callback_for_event(element, 'explore');
+		try {
+
+			// try to assign callback
+			assign_callback_for_event(element, 'explore');
 
 
-	} catch (err) {
+		} catch (err) {
 
-		console.log("error while trying to assign callback: " + err);
+			console.log("error while trying to assign callback: " + err);
+
+		}
 
 	}
+
 }
 
 // now foreach layer below the topmost, assign_callback with custom callback name
@@ -75,4 +96,8 @@ images.forEach(assign_callback);
 
 // now add hover callback for the topmost layer
 var topmost_layer = images[images.length - 1];
-assign_callback_for_event(topmost_layer, 'mouseover');
+var topmost_layer_name = layer_name(topmost_layer.id);
+console.log('topmost layer name:' + topmost_layer_name);
+$(topmost_layer_name).on("mouseover", function(event) {
+	$(topmost_layer_name).trigger('explore');
+});
