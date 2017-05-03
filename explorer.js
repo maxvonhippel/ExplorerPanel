e329dbe6dec1f1ea52cd5f46c66c9ef774669d76
Max von Hippel <maxvonhippel1996@gmail.com>
2017-05-03 09:08:47 -0700

    got click on img by alpha to work

--- explorer.js
+++ explorer.js
@@ -1,60 +1,34 @@
-// add the images
-var explorer = document.getElementById("explorer");
-var cvs = document.createElement('canvas');
-// find the canvas context
-var ctx = document.getElementById('canvas').getContext('2d');
+// create the canvas
+var canvas = document.createElement("canvas");
+// add the canvas
+canvas.setAttribute("id", "cid");
+document.body.appendChild(canvas);
+// get the context
+var ctx = canvas.getContext("2d");
 
-// first image
-var img1 = new Image();
-img1.onload = function() {
-    ctx.drawImage(img,0,0);
-};
-img1.src = "photos/layer_1_demo.png";
+// handle the clicks
+// code from http://stackoverflow.com/a/38488246/1586231
+$('#layer_1').on("mousedown", function(event) {
+	// Get click coordinates
+	var x = event.pageX - this.offsetLeft,
+		y = event.pageY - this.offsetTop,
+		w = ctx.canvas.width = this.width,
+		h = ctx.canvas.height = this.height,
+		alpha;
 
-var isTransparentUnderMouse = function (target, evnt) {
-    var l = 0, t = 0;
-    if (target.offsetParent) {
-        var ele = target;
-        do {
-            l += ele.offsetLeft;
-            t += ele.offsetTop;
-        } while (ele = ele.offsetParent);
-    }
-    var x = evnt.page.x - l;
-    var y = evnt.page.y - t;
-    var imgdata = target.getContext('2d').getImageData(x, y, 1, 1).data;
-    if (
-        imgdata[0] == 0 &&
-        imgdata[1] == 0 &&
-        imgdata[2] == 0 &&
-        imgdata[3] == 0
-    ){
-        return true;
-    }
-    return false;
-};
-/*
-img1.class = "imgA1";
-img1.src = "photos/layer_1_demo.png";
-img1.style.maxHeight = "50%";
-img1.style.maxWidth = "50%";
-explorer.appendChild(img1);
+	// Draw image to canvas
+	// and read Alpha channel value
+	ctx.drawImage(this, 0, 0, w, h);
+	alpha = ctx.getImageData(x, y, 1, 1).data[3]; // [0]R [1]G [2]B [3]A
 
-// second image
-var img2 = new Image();
-img2.class = "imgA2";
-img2.src = "photos/layer_2_demo.png";
-img2.style.maxHeight = "50%";
-img2.style.maxWidth = "50%";
-explorer.appendChild(img2);
-
-// log boxes
-// see this: http://stackoverflow.com/questions/11228987/image-map-by-alpha-channel
-
-img2.onmouseover = function(event) {
-	console.log("over");
-};
-
-img1.onmouseover = function() {
-	console.log("over");
-};*/
+	// If pixel is transparent,
+	// retrieve the element underneath and trigger it's click event
+	if( alpha === 0 ) {
+		/*
+		 *	If this occurs, we need to try triggering click on the element below this component,
+		 *  and continue thus down the stack.
+		 */
+	} else {
+		console.log("LOGO clicked!");
+	}
+});
