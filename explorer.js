ab45c0003161aa70a4f1048bf8ef1c48b0800473
Max von Hippel <maxvonhippel1996@gmail.com>
2017-05-05 22:00:50 -0700

    did some work with map(), going to move back to some sort of array

--- explorer.js
+++ explorer.js
@@ -1,7 +1,7 @@
 var canvas = document.createElement("canvas");
 // add the canvas
 var context = canvas.getContext('2d');
-var currently_drawn = 0;
+var currently_drawn = '';
 
 // name with hashtag to find div of a layer
 function layer_name(name) {
@@ -25,8 +25,7 @@ function toArray(a) {
 
 // find all layers in our diagram
 var container = document.getElementById("container");
-var images = toArray(container.getElementsByClassName("layer"));
-
+var images = new Map();
 function assign_callback_for_event(element, event_name) {
 
 	// code from http://stackoverflow.com/a/38488246/1586231
@@ -44,9 +43,10 @@ function assign_callback_for_event(element, event_name) {
 
 			// Draw image to canvas
 			// and read Alpha channel value
-			if (currently_drawn != images.indexOf(this)) {
+			if (currently_drawn != this.id) {
 				context.drawImage(this, 0, 0, w, h);
-				currently_drawn = images.indexOf(this);
+				currently_drawn = this.id;
+				console.log("drawing new image for layer: ", currently_drawn);
 			}
 			alpha = context.getImageData(x, y, 1, 1).data[3]; // [0]R [1]G [2]B [3]A
 			console.log(x, y, w, h, "alpha: " + alpha, this.id);
@@ -55,11 +55,15 @@ function assign_callback_for_event(element, event_name) {
 			if( alpha === 0 ) {
 
 				// maybe worth attempting in the future: http://stackoverflow.com/a/13426070/1586231
-				var cur_index = images.indexOf(this);
+				var cur_index = images.prototype.keys().indexOf(this.id);
+				console.log("cur_index: ", cur_index);
 				if (cur_index > 0) {
 					cur_index--;
-					var next_layer = images[cur_index];
+					var next_layer_id = images.prototype.keys()[cur_index];
+					console.log("next_layer_id: ", next_layer_id);
+					var next_layer = images.get(next_layer_id);
 					var e = new jQuery.Event("mousedown");
+					console.log("triggering: ", event.pageX, event.pageY);
 					e.pageX = event.pageX;
 					e.pageY = event.pageY;
 					$(layer_name(next_layer.id)).trigger(e);
@@ -75,16 +79,25 @@ function assign_callback_for_event(element, event_name) {
 
 function assign_callback(element, index, array) {
 
-	element.onload = function() {
-		try {
-			// try to assign callback
-			assign_callback_for_event(element, "mousedown");
-		} catch (err) {
-			console.log("error while trying to assign callback: " + err);
+	if (index != 0) {
+		element.onload = function() {
+			try {
+				// try to assign callback
+				assign_callback_for_event(element, "mousedown");
+			} catch (err) {
+				console.log("error while trying to assign callback: " + err);
+			}
 		}
 	}
-
 }
 
-// now foreach layer below the topmost, assign_callback with custom callback name
-images.forEach(assign_callback);
+function image_assign(element, index, array) {
+	console.log(element.id, index);
+	images.set([element.id], element);
+	assign_callback(element, index, array);
+}
+
+var images_array = toArray(container.getElementsByClassName("layer"));
+images_array.forEach(image_assign);
+
+assign_callback_for_event(images.get('layer_1'), "mousemove");
