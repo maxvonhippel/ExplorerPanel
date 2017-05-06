3720e62ffe5956a37c812ba6859b0ba035dd268a
Max von Hippel <maxvonhippel1996@gmail.com>
2017-05-05 18:47:33 -0700

    getting closer to automatic setup, though animations and hover stuff still left to implement

--- explorer.js
+++ explorer.js
@@ -1,6 +1,5 @@
 var canvas = document.createElement("canvas");
 // add the canvas
-canvas.setAttribute("id", "cid");
 var context = canvas.getContext('2d');
 
 // name with hashtag to find div of a layer
@@ -26,17 +25,14 @@ function toArray(a) {
 // find all layers in our diagram
 var container = document.getElementById("container");
 var images = toArray(container.getElementsByClassName("layer"));
-// images[0] is bottom of stack of images, last image is frontmost layer
 
 function assign_callback_for_event(element, event_name) {
 
 	// code from http://stackoverflow.com/a/38488246/1586231
 	var _name = layer_name(element.id);
 
-	console.log("assigning callback: " + element.id);
-
 	$(_name).on(event_name, function(event) {
-
+			console.log("checking: ", this.id);
 			// Get click coordinates
 			var x = event.pageX - this.offsetLeft,
 				y = event.pageY - this.offsetTop,
@@ -47,21 +43,24 @@ function assign_callback_for_event(element, event_name) {
 
 			// Draw image to canvas
 			// and read Alpha channel value
-
 			context.drawImage(this, 0, 0, w, h);
-			rgba = context.getImageData(x, y, 1, 1);
-			console.log("x: " + x + " y: " + y + " w: " + w + " h: " + h + " rgba: " + rgba);
-			alpha = rgba.data[3]; // [0]R [1]G [2]B [3]A
-
-			console.log(alpha);
+			alpha = context.getImageData(x, y, 1, 1).data[3]; // [0]R [1]G [2]B [3]A
 
 			// If pixel is transparent,
 			// retrieve the element underneath and trigger it's click event
 			if( alpha === 0 ) {
 
-				$(this).hide();
-				$(document.elementFromPoint(event.clientX, event.clientY)).trigger('explore');
-				$(this).show();
+				// maybe worth attempting in the future: http://stackoverflow.com/a/13426070/1586231
+				var cur_index = images.indexOf(this);
+				if (cur_index > 0) {
+					cur_index--;
+					var next_layer = images[cur_index];
+					console.log(next_layer.id);
+					var e = new jQuery.Event("mousedown");
+					e.pageX = event.clientX;
+					e.pageY = event.clientY;
+					$(layer_name(next_layer.id)).trigger(e);
+				}
 
 			} else {
 				console.log("CLICKED: " + this.id);
@@ -74,30 +73,15 @@ function assign_callback_for_event(element, event_name) {
 function assign_callback(element, index, array) {
 
 	element.onload = function() {
-
 		try {
-
 			// try to assign callback
-			assign_callback_for_event(element, 'explore');
-
-
+			assign_callback_for_event(element, "mousedown");
 		} catch (err) {
-
 			console.log("error while trying to assign callback: " + err);
-
 		}
-
 	}
 
 }
 
 // now foreach layer below the topmost, assign_callback with custom callback name
 images.forEach(assign_callback);
-
-// now add hover callback for the topmost layer
-var topmost_layer = images[images.length - 1];
-var topmost_layer_name = layer_name(topmost_layer.id);
-console.log('topmost layer name:' + topmost_layer_name);
-$(topmost_layer_name).on("mouseover", function(event) {
-	$(topmost_layer_name).trigger('explore');
-});
