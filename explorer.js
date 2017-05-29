be2440b7acf483fda6dc10c61eb218fffaa66979
Max von Hippel <maxvonhippel1996@gmail.com>
2017-05-29 15:23:08 -0700

    Revert "compressed everything into one stylesheet"
    This reverts commit 5af28d421b2396fedb19a76de83c144d33033faa.

--- explorer.js
+++ explorer.js
@@ -1,3 +1,85 @@
+// add the stylesheets
+var styles = document.createElement('link');
+styles.rel = 'stylesheet';
+styles.type = 'text/css';
+styles.href = 'Remodal/dist/remodal.css';
+var def_styles = document.createElement('link');
+def_styles.rel = 'stylesheet';
+def_styles.type = 'text/css';
+def_styles.href = 'Remodal/dist/remodal-default-theme.css';
+var rem_script = document.createElement('script');
+rem_script.src = "Remodal/dist/remodal.min.js";
+var head = document.getElementsByTagName('head')[0];
+var further_styles =`
+.remodal-overlay {
+	opacity: 0;
+}
+.remodal {
+	color: #2b2e38;
+	background: rgba(255, 255, 255, 0.7);
+	max-width: 55%;
+	max-height: 55%;
+}
+.remodal-bg.remodal-is-opening,
+.remodal-bg.remodal-is-opened {
+	-webkit-filter: blur(5px);
+	filter: blur(5px);
+}
+.remodal-overlay.remodal-is-opening,
+.remodal-overlay.remodal-is-closing {
+	-webkit-animation-duration: 0s;
+	animation-duration: 0s;
+	-webkit-animation-fill-mode: forwards;
+	animation-fill-mode: forwards;
+}
+.remodal-overlay.remodal-is-opening {
+	-webkit-animation-name: remodal-overlay-opening-keyframes;
+	animation-name: remodal-overlay-opening-keyframes;
+}
+.remodal-overlay.remodal-is-closing {
+	-webkit-animation-name: remodal-overlay-closing-keyframes;
+	animation-name: remodal-overlay-closing-keyframes;
+}
+@-webkit-keyframes remodal-overlay-opening-keyframes {
+	from {
+		opacity: 0;
+	}
+	to {
+		opacity: 0;
+	}
+}
+@keyframes remodal-overlay-opening-keyframes {
+	from {
+		opacity: 0;
+	}
+	to {
+		opacity: 0;
+	}
+}
+@-webkit-keyframes remodal-overlay-closing-keyframes {
+	from {
+		opacity: 0;
+	}
+	to {
+		opacity: 0;
+	}
+}
+
+@keyframes remodal-overlay-closing-keyframes {
+	from {
+		opacity: 0;
+	}
+	to {
+		opacity: 0;
+	}
+}
+`;
+var further_styles_div = document.createElement('style');
+further_styles_div.innerHTML = further_styles;
+head.appendChild(styles);
+head.appendChild(def_styles);
+head.appendChild(rem_script);
+head.appendChild(further_styles_div);
 // find the container for the interactive
 var container = document.getElementById("container");
 
